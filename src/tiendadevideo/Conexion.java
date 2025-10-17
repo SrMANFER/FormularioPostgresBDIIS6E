740e697291029d87ac14d7aae14c471f5b129c91
SrMANFER <m4nf3r@gmail.com>
2025-10-16 21:37:01 -0500

    Inicio Modificación

---
+++ src/tiendadevideo/Conexion.java
@@ -0,0 +1,65 @@
+package tiendadevideo;
+
+import java.sql.*;
+
+public class Conexion {
+//String driver="com.mysql.jdbc.Driver";
+       
+    String driver = "org.postgresql.Driver";
+       
+    //String url="jdbc:mysql://localhost:3306/tiendadevideos2"; /** este nombre es el nombre de la base de datos? */
+       
+    String url= "jdbc:postgresql://localhost:5432/tienda_video";
+   
+    String user,contra,aviso1,aviso2;
+int indica1,indica2;
+       
+Connection con=null;
+Statement stmt=null;
+
+   public Conexion(String user, String contra) {
+this.user=user;  indica1=0; indica2=0;
+try {
+     Class.forName(driver);
+     System.out.println("Se ha cargado correctamente el driver:"+driver);
+     aviso1="Se ha cargado correctamente el driver: "+driver;
+     indica1=1;
+}
+catch (Exception e){
+  System.out.println("Se ha producido el siguiente error al cargar el driver:\n"+e.getMessage());
+}
+try{
+    con=DriverManager.getConnection(url,user,contra);
+             
+    System.out.println("Se ha conectado correctamente a la base de datos");
+    System.out.println("Conection ==>"+(Object)con);
+    aviso1="Se ha conectado correctamente a la base de datos";
+    indica2=1;
+}
+catch(Exception e){
+   System.out.println("Se ha producido un error al momento de conectar la base de datos: \n"+e.getMessage());
+}
+        if (indica2==1){
+           try {
+                 stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+           }catch (SQLException e) {
+                   System.out.println ("Error Creando el Statement= "+ e.getMessage());
+            }
+        }else{
+               System.out.println("Invalida usuario o contraseña");
+         }
+    }
+
+    public Connection con(){ return con;  }
+
+    public Statement stamt(){ return stmt; }
+
+    public String rAviso1(){ return aviso1; }
+
+    public String rAviso2(){ return aviso2; }
+
+    public int indica_1(){ return indica1; }
+
+    public int indica_2(){ return indica2; }
+}
+
